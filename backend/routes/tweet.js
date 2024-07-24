946346846b5526d8c6c873866f3bfd82a49eb7d8
Amanda <amanda.ausejo@gmail.com>
2024-07-24 11:12:11 +0200

    Improve error handling by including all logic in try-catch blocks

--- backend/routes/tweet.js
+++ backend/routes/tweet.js
@@ -12,18 +12,17 @@ router.get("/", (req, res) => {
     return res.status(200).json(tweets);
   } catch (error) {
     console.error(error.stack);
-    res.status(500).json({ message: error.message });
+    return res.status(500).json({ message: error.message });
   }
 });
 
 //post new tweet
 router.post("/", (req, res) => {
-  const { username, text } = req.body;
-  if (!username || !text) {
-    return res.status(400).json("You must insert a username and a text to create a tweet");
-  }
-
   try {
+    const { username, text } = req.body;
+    if (!username || !text) {
+      return res.status(400).json("You must insert a username and a text to create a tweet");
+    }
     const newTweet = new Tweet(nanoid(20), username, text);
     tweets.push(newTweet);
     return res.status(201).json(newTweet);
@@ -35,17 +34,17 @@ router.post("/", (req, res) => {
 
 //like the tweet
 router.post("/:id/like", (req, res) => {
-  const likedTweet = tweets.find((t) => t.id === req.params.id);
-  if (likedTweet) {
-    try {
+  try {
+    const likedTweet = tweets.find((t) => t.id === req.params.id);
+    if (likedTweet) {
       likedTweet.likes += 1;
       return res.status(201).json(likedTweet);
-    } catch (error) {
-      console.error(error.stack);
-      res.status(500).json({ message: error.message });
+    } else {
+      return res.status(400).json(`Tweet with id ${req.params.id} not found`);
     }
-  } else {
-    res.status(404).json({ error: "Tweet not found" });
+  } catch (error) {
+    console.error(error.stack);
+    return res.status(500).json({ message: error.message });
   }
 });
 
